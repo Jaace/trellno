4cfa16782544847ff11bb5310502855d63760150
Jason <jason.boyle@nytimes.com>
2021-03-13 19:49:49 -0500

    Card creation, save and cancel keyboard controls

--- src/Cards.svelte
+++ src/Cards.svelte
@@ -9,37 +9,58 @@
     cards.update((cards) => [...cards, card]);
   }
 
-  function createCard() {
+  function saveCard() {
     if (card.title) {
       addCard(card);
-    } else {
-      throw 'You must enter a title for this card.';
+      card = { listId };
     }
-    toggleCreate();
+  }
+
+  function cancelCard() {
     card = { listId };
+    toggleCreate();
   }
 
   function toggleCreate() {
     createCardForm = !createCardForm;
   }
+
+  function keyboardControls(e) {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      return saveCard();
+    }
+
+    if (e.key === 'Escape') {
+      e.preventDefault();
+      return cancelCard();
+    }
+  }
 </script>
 
 <ul>
   {#each $cards as card}
-    {#if card.listId === listId }
-    <li contenteditable bind:textContent={card.title}></li>
+    {#if card.listId === listId}
+      <li contenteditable bind:textContent={card.title} />
     {/if}
   {/each}
   {#if createCardForm}
-    <li contenteditable bind:textContent={card.title}></li>
+    <!-- svelte-ignore a11y-autofocus -->
+    <li
+      contenteditable
+      bind:textContent={card.title}
+      autofocus
+      on:keydown={keyboardControls}
+      placeholder="Enter a title for this card..."
+    />
   {/if}
 </ul>
 
 {#if !createCardForm}
   <button on:click={toggleCreate}>Create Card</button>
 {:else}
-  <button on:click={toggleCreate}>Cancel</button>
-  <button on:click={createCard}>Save</button>
+  <button on:click={cancelCard}>Cancel</button>
+  <button on:click={saveCard}>Save</button>
 {/if}
 
 <style>
@@ -56,5 +77,10 @@
     border-radius: 3px;
     box-shadow: 0 1px 0 rgba(9, 30, 66, 0.25);
     padding: 10px;
+  }
+
+  li[placeholder]:empty:before {
+    color: rgba(0, 0, 0, 0.5);
+    content: attr(placeholder);
   }
 </style>
