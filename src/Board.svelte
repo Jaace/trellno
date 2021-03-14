e2bfef73be0ac4bacb50baec6f7798738dc99fca
Jason <jason.boyle@nytimes.com>
2021-03-13 19:24:45 -0500

    Implement drag-and-drop re-ordering on lists

--- src/Board.svelte
+++ src/Board.svelte
@@ -1,9 +1,13 @@
 <script>
+  import { flip } from 'svelte/animate';
+  import { dndzone } from 'svelte-dnd-action';
+
   import { lists } from './stores.js';
   import Cards from './Cards.svelte';
 
   let listCreateForm = false;
   let list = {};
+  const flipDurationMs = 200;
 
   function createList() {
     list.id = Math.random();
@@ -15,12 +19,23 @@
   function toggleCreate() {
     listCreateForm = !listCreateForm;
   }
+
+  function handleDndConsiderColumns(e) {
+    $lists = e.detail.items;
+  }
+  function handleDndFinalizeColumns(e) {
+    $lists = e.detail.items;
+  }
 </script>
 
-<ul>
-  {#each $lists as list}
-    <li>
-      <h2 contenteditable bind:textContent={list.title}></h2>
+<ul
+  use:dndzone={{ items: $lists, flipDurationMs, type: 'columns' }}
+  on:consider={handleDndConsiderColumns}
+  on:finalize={handleDndFinalizeColumns}
+>
+  {#each $lists as list (list.id)}
+    <li animate:flip={{ duration: flipDurationMs }}>
+      <h2 contenteditable bind:textContent={list.title} />
       <Cards listId={list.id} />
     </li>
   {/each}
@@ -28,17 +43,20 @@
     {#if !listCreateForm}
       <button on:click={toggleCreate}>Add List</button>
     {:else}
-      <h2 contenteditable bind:textContent={list.title}></h2>
+      <h2 contenteditable bind:textContent={list.title} />
       <button on:click={toggleCreate}>Cancel</button>
       <button on:click={createList}>Save</button>
     {/if}
   </li>
 </ul>
 
-
-
 <style>
   ul {
+    height: 90vh;
+    width: 100%;
+    padding: 0.5em;
+    margin-bottom: 40px;
+
     display: grid;
     grid-auto-columns: 272px;
     grid-auto-flow: column;
